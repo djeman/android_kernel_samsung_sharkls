10def37ff9e163686c0e4755cd3914c5d75fa2fd
djeman <djemouli@gmail.com>
2019-12-24 20:01:14 +0100

    i2c: fix of driver
    Change-Id: I97ccf9027d659090672de442777d00a8688ec208

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -157,7 +157,7 @@ static void * unflatten_dt_node(struct boot_param_header *blob,
 				__alignof__(struct device_node));
 	if (allnextpp) {
 		char *fn;
-		of_node_init(np);
+		memset(np, 0, sizeof(*np));
 		np->full_name = fn = ((char *)np) + sizeof(*np);
 		if (new_format) {
 			/* rebuild full path for new format */
@@ -188,6 +188,7 @@ static void * unflatten_dt_node(struct boot_param_header *blob,
 				dad->next->sibling = np;
 			dad->next = np;
 		}
+		kref_init(&np->kref);
 	}
 	/* process properties */
 	for (offset = fdt_first_property_offset(blob, *poffset);
@@ -335,8 +336,6 @@ static void __unflatten_device_tree(struct boot_param_header *blob,
 
 	/* Allocate memory for the expanded device tree */
 	mem = dt_alloc(size + 4, __alignof__(struct device_node));
-
-	memset((void *)mem, 0, size);
 
 	memset((void *)mem, 0, size);
 
